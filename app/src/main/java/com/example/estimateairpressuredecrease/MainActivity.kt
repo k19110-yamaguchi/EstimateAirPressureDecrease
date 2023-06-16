6b8b12c80c714b6fdbdf31cd07eeb52b365a7e8e
k19110-yamaguchi <69891399+k19110-yamaguchi@users.noreply.github.com>
2023-06-16 15:45:26 +0900

    project新規作成

---
+++ app/src/main/java/com/example/estimateairpressuredecrease/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.estimateairpressuredecrease
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.estimateairpressuredecrease.ui.theme.EstimateAirPressureDecreaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EstimateAirPressureDecreaseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EstimateAirPressureDecreaseTheme {
+        Greeting("Android")
+    }
+}
